16a1a3dea7f214c891d5656f9b0caa1bd25c693b
unknown <gandurinaresh@gmail.com>
2019-05-05 14:11:50 -0500

    update to use console crate (and colors!)

--- src/io.rs
+++ src/io.rs
@@ -3,7 +3,9 @@ use crate::game::Marker;
 use crate::mark::Mark;
 use crate::player::Player;
 use crate::square::Square;
-use std::io::{self, Write};
+use std::io::Write;
+
+use console::{style, Term};
 
 static X_MARKER: [&str; 5] = [
     "##     ##",
@@ -22,31 +24,29 @@ static O_MARKER: [&str; 5] = [
 ];
 
 pub fn print_header(msg: &str) {
-    println!("{}", msg);
-    println!("=========================================");
+    let mut term = Term::stdout();
+
+    writeln!(term, "{}", style(msg).blue()).unwrap();
+
+    let bar = style("=========================================").blue();
+    writeln!(term, "{}", bar).unwrap();
 }
 
 pub fn clear_screen() {
-    for _ in 0..150 {
-        println!();
-    }
+    let term = Term::stdout();
+    term.clear_screen().unwrap();
 }
 
 pub fn wait() {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
-    let mut input = String::new();
+    let mut term = Term::stdout();
 
-    print!("Press enter to continue...");
-    stdout.flush().unwrap();
-    stdin.read_line(&mut input).unwrap();
+    write!(term, "Press enter to continue...").unwrap();
+    term.read_line().unwrap();
 }
 
 pub fn get_yes_no(msg: &str) -> bool {
-    let mut input = String::new();
-
     let msg = format!("{} [y/n]: ", msg);
-    get_prompt(&msg, &mut input);
+    let mut input = get_prompt(&msg);
 
     loop {
         input.make_ascii_lowercase();
@@ -56,26 +56,20 @@ pub fn get_yes_no(msg: &str) -> bool {
         } else if input == "no" || input == "n" {
             return false;
         } else {
-            get_prompt("Invalid input. Try again [y/n]: ", &mut input);
+            input = get_prompt("Invalid input. Try again [y/n]: ");
         }
     }
 }
 
-fn get_prompt(msg: &str, input: &mut String) {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
-    input.clear();
+fn get_prompt(msg: &str) -> String {
+    let mut term = Term::stdout();
 
-    print!("{}", msg);
-    stdout.flush().unwrap();
-    stdin.read_line(input).unwrap();
-    input.retain(|c| !c.is_whitespace());
+    write!(term, "{}", msg).unwrap();
+    term.read_line().unwrap()
 }
 
 pub fn get_player_move(board: Board) -> Mark {
-    let mut input = String::new();
-
-    get_prompt("Enter a number [0-8]: ", &mut input);
+    let mut input = get_prompt("Enter a number [0-8]: ");
 
     loop {
         if let Ok(index) = input.parse::<usize>() {
@@ -83,17 +77,14 @@ pub fn get_player_move(board: Board) -> Mark {
             let Mark { x, y } = mark;
 
             if index >= 9 {
-                get_prompt(
-                    "Input must be between 0 and 8. Try again. [0-8]:",
-                    &mut input,
-                );
+                input = get_prompt("Input must be between 0 and 8. Try again. [0-8]:");
             } else if board.get(x, y).is_empty() {
                 return mark;
             } else {
-                get_prompt("Spot is already taken. Try again. [0-8]: ", &mut input);
+                input = get_prompt("Spot is already taken. Try again. [0-8]: ");
             }
         } else {
-            get_prompt("Invalid input. Try again. [0-8]: ", &mut input);
+            input = get_prompt("Invalid input. Try again. [0-8]: ");
         }
     }
 }
@@ -122,40 +113,45 @@ pub fn get_comp_move(board: Board) -> Mark {
 }
 
 pub fn render(board: Board, player_marker: Marker) {
+    let mut term = Term::stdout();
     let (player_marker, comp_marker) = match player_marker {
         Marker::Cross => (X_MARKER, O_MARKER),
         Marker::Naught => (O_MARKER, X_MARKER),
     };
 
-    println!("+---------+---------+---------+");
+    term.write_line("+---------+---------+---------+").unwrap();
 
     for x in 0..3 {
         for line in 0..5 {
             for y in 0..3 {
                 let spot = board.get(x, y);
                 match spot {
-                    Square::Human => print!("|{}", player_marker[line]),
-                    Square::Computer => print!("|{}", comp_marker[line]),
+                    Square::Human => {
+                        let line = style(player_marker[line]).green();
+                        write!(term, "|{}", line).unwrap()
+                    }
+                    Square::Computer => {
+                        let line = style(comp_marker[line]).red();
+                        write!(term, "|{}", line).unwrap()
+                    }
                     Square::Empty => {
                         if line == 0 {
                             let index = x * 3 + y;
-                            print!("|{}        ", index);
+                            write!(term, "|{}        ", index).unwrap();
                         } else {
-                            print!("|         ");
+                            write!(term, "|         ").unwrap();
                         }
                     }
                 }
             }
-            println!("|");
+            term.write_line("|").unwrap();
         }
-        println!("+---------+---------+---------+");
+        term.write_line("+---------+---------+---------+").unwrap();
     }
 }
 
 pub fn get_marker() -> Marker {
-    let mut input = String::new();
-
-    get_prompt("Choose marker [X/O]: ", &mut input);
+    let mut input = get_prompt("Choose marker [X/O]: ");
     input.make_ascii_lowercase();
 
     loop {
@@ -164,7 +160,7 @@ pub fn get_marker() -> Marker {
         } else if input == "o" {
             return Marker::Naught;
         } else {
-            get_prompt("Invalid input. Try again. [X/O]: ", &mut input);
+            input = get_prompt("Invalid input. Try again. [X/O]: ");
         }
     }
 }
